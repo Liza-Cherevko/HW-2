4667a2fc024875e0b21233d224b609746d8a3da9
Liza <cherevkoelp@gmail.com>
2022-04-15 21:55:46 +0300

    HW-2

---
+++ script.js
@@ -0,0 +1,54 @@
+// №1
+// var min = prompt()
+// if(min >0 && min <=14){
+//     console.log('в первую четверть')
+// } else if (min >= 15 && min <= 30) {
+//     console.log('вo вторую четверть')
+// }
+// else if (min >= 31 && min <= 45) {
+//     console.log('в  третью четверть')
+// }
+// else if (min >= 46 && min <= 60) {
+//     console.log('в  четвертую четверть')
+// }
+
+// №2
+// var a = prompt()
+// if(a==1){
+//     console.log("верно")
+// } else{
+//     console.log("не верно")
+// }
+
+
+// №3 ??
+// var test = promt()
+var test = 0;
+
+if(test != true){
+    console.log('верно')
+} else{
+console.log('не верно')
+}
+
+// №4
+
+// var a = prompt() 
+// if(a > 0){
+//     console.log('верно')
+// }  else if (a <5){
+//     console.log('не верно')
+// }
+
+// №5
+// var num =prompt() 
+
+// if(num==1){
+//     console.log('зима')
+// } else if (num == 2){
+//     console.log('весна')
+// }else if(num == 3){
+//     console.log('лето')
+// }else if(num == 4){
+//     console.log('осень')
+// } 
